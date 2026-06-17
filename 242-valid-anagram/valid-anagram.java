a9fff0e5d56dfbd11b724474f016da3148667ab7
SNEKA S <sneka3920@gmail.com>
2026-06-17 10:40:17 +0530

    Time: 3 ms (92.08%) | Memory: 46.7 MB (8.90%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        String a=new String(ch1);
+        String b=new String(ch2);
+        if(s.length()!=t.length()){
+            return false;
+        }
+        return a.equals(b);
+    }
+}
