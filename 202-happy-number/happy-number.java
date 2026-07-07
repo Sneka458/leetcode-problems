d5707e57eb19cc7d499478d6a8283c948333ef1d
SNEKA S <sneka3920@gmail.com>
2026-07-07 21:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (51.35%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,16 @@
+//202.Happy Number
+
+class Solution {
+    public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            int sum = 0;
+            while (n > 0) {
+                int m = n % 10;
+                sum += m * m;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+}
