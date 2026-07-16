b11cf191a5673ff5faae5b4997e0588239b06ae4
SNEKA S <sneka3920@gmail.com>
2026-07-16 19:31:49 +0530

    Time: 14 ms (90.39%) | Memory: 43.4 MB (89.50%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int start=0,end=0;
+        if(s==null || s.length()<1) return "";
+        for(int i=0;i<s.length();i++){
+           int l1= f(s,i,i);//odd
+           int l2= f(s,i,i+1);//even
+           int len =Math.max(l1,l2);
+        if(len>end-start){
+            start=i-(len-1)/2;
+            end=i+len/2;
+        }
+    }
+        return s.substring(start,end+1);
+    }
+    public int f(String s,int l,int r){
+        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            l--;r++;
+        }
+        return r-l-1;
+    }
+}
